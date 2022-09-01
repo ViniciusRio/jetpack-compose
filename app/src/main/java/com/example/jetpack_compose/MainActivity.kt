4420853e76be199c6fbc01f1ae3987fa83cefd04
ViniciusRio <viniciustrave@gmail.co>
2022-08-31 22:06:18 -0300

    Add new image and alignment text

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_compose.ui.theme.JetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +29,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithImage()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column() {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun BirthdayGreetingWithImage() {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = stringResource(R.string.happy_birthday), from = stringResource(
+            R.string.from_message)
+        )
+    }
+}
+
+/*
+* Must a be a noun: DoneButton
+* Not a verb: DrawExtField
+* Not nouned preposition: TextFieldWithLink
+* not adjective: Bright
+* not a adverb: Outside
+* Nouns may be prefixed by descriptive adjectives: RoundIcon
+*
+* */
+@Preview(showBackground = false, name = "Default Preview", showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JetpackcomposeTheme {
-        Greeting("Android")
+        BirthdayGreetingWithImage()
     }
 }
