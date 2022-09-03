201e7256670210b39989a916e9bfcebc6278343f
ViniciusRio <viniciustrave@gmail.com>
2022-09-03 15:27:39 -0300

    Revert "Understand row weight and size of elements"
    This reverts commit 86239dcb2880d7d301436b36782786e4efbbfab3.

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -3,20 +3,13 @@ package com.example.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose.ui.theme.JetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,78 +17,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackcomposeTheme {
-                Surface (color = MaterialTheme.colors.background){
-                    ComposableQuadrant()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
                 }
             }
         }
     }
 }
+
 @Composable
-fun ComposableQuadrant() {
-    Column(Modifier.fillMaxWidth()) {
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(id = R.string.textComposable),
-                description = stringResource(id = R.string.textComposableDescription),
-                backgroundColor = Color.Green,
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = stringResource(id = R.string.imageComposable),
-                description = stringResource(id = R.string.imageComposableDescription),
-                backgroundColor = Color.Yellow,
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(id = R.string.rowComposable),
-                description = stringResource(id = R.string.rowComposableDescription),
-                backgroundColor = Color.Cyan,
-                modifier = Modifier.weight(1f)
-            )
-            ComposableInfoCard(
-                title = stringResource(id = R.string.columnComposable),
-                description = stringResource(id = R.string.textComposableDescription),
-                backgroundColor = Color.LightGray,
-                modifier = Modifier.weight(1f)
-            )
-        }
-    }
-}
-@Composable
-private fun ComposableInfoCard(
-    title: String,
-    description: String,
-    backgroundColor: Color,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            // makes element fill all size available
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = description,
-            textAlign = TextAlign.Justify
-        )
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackcomposeTheme {
-        ComposableQuadrant()
+        Greeting("Android")
     }
 }
