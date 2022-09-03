7129b2fdb7fcdaaf8cd9402d568cc262bd5729c2
ViniciusRio <viniciustrave@gmail.com>
2022-09-03 12:20:28 -0300

    Understand horizontal alignment and vertical Arrangement. Font weight bold

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_compose.ui.theme.JetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +26,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeTaskManager()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeTaskManager() {
+    TaskManager(
+        statusMessage = stringResource(R.string.statusMessage),
+        customMessage = stringResource(R.string.customMessage),
+        painterImage = painterResource(id = R.drawable.ic_task_completed),
+    )
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun TaskManager(
+    statusMessage: String,
+    customMessage: String,
+    painterImage: Painter,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterImage,
+            contentDescription = null
+        )
+        Text(
+            text = statusMessage,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = customMessage,
+            fontSize = 16.sp,
+        )
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JetpackcomposeTheme {
-        Greeting("Android")
+        ComposeTaskManager()
     }
 }
